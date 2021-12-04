16aa6fbff8b6f066b5a2e7ba6fe2e201de2324ed
ViniciusAzevedoM <viniciusm_azevedo@hotmail.com>
2021-12-04 16:29:41 -0300

    componenting footer

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div name="Footer" className="flex justify-center h-20 bg-white">
+            <p className="self-center text-lifoo-100 text-2xl">Informações do rodapé</p>
+        </div>
+    );
+}
+
+export default Footer
