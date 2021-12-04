886d088d8fed83122f791095bdc2665b049fb5a0
ViniciusAzevedoM <viniciusm_azevedo@hotmail.com>
2021-12-04 16:28:38 -0300

    adding the rest of structure of home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,91 @@
 import React from 'react';
+import Footer from '../components/footer'
 
 function Home() {
   return (
     <div>
-      <div name="navbar" className="w-full h-14 bg-lifoo-200 flex justify-end">
-        <button className="text-sm text-white mr-10">Home</button>
-        <button className="text-sm text-white mr-10">Sobre</button>
-        <button className="text-sm text-white mr-10">Contato</button>
-        <button className="self-center w-40 h-9 bg-white rounded-2xl text-sm mr-20">Cadastre seu perfil</button>
+      <div name="Navbar" className="w-full h-20 bg-lifoo-200 flex justify-end">
+        <button className="text-md text-white mr-10">Home</button>
+        <button className="text-md text-white mr-10">Sobre</button>
+        <button className="text-md text-white mr-10">Contato</button>
+        <button className="self-center w-40 h-9 bg-white rounded-2xl text-md mr-20">Cadastre seu perfil</button>
       </div>
-      <div name="mainDiv" className="w-full h-96 bg-white flex flex-col flex-wrap justify-center">
-        <div className="ml-28">
+      <div name="MainDiv" className="w-full h-96 bg-white grid grid-cols-2">
+        <div className="-ml-56 place-self-center">
           <p className="text-lifoo-100 text-5xl mb-10"><b>Ajude a reduzir<br /> a fome no<br /> mundo!</b></p>
           <button className="w-48 h-11 bg-lifoo-100 rounded-3xl text-white text-sm mr-20"><b>Quero ajudar!</b></button>
         </div>
-        <div className="w-2/3 h-full bg-black">
-            <p className="text-3xl text-white">Inserir foto</p>
+        <div className="w-100% h-full -ml-40 bg-black">
+          <p className="text-3xl text-white">Inserir foto</p>
         </div>
       </div>
+      <div name="About" className="py-20 bg-gradient-to-r from-lifoo-100 to-lifoo-300 grid grid-cols-2">
+        <div className="place-self-center">
+          <p className="text-white text-5xl mb-10">
+            <b>O que é a <br />Lifoo?</b>
+          </p>
+          <p className="text-white text-2xl">
+            Lorem impsun, Lorem impsun, Lorem impsun,<br />
+            Lorem impsun, Lorem impsun, Lorem impsun,<br />
+            Lorem impsun, Lorem impsun, Lorem impsun,<br />
+            Lorem impsun, Lorem impsun, Lorem impsun.<br />
+          </p>
+        </div>
+        <div className="place-self-center">
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/IN7JB0GtzHc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+      </div>
+      <div name="Process" className="bg-white mt-20 mb-20">
+        <div className="">
+          <p className="text-lifoo-100 text-5xl ml-60 mb-20"><b>Entenda<br /> o processo</b></p>
+          <div className="flex justify-around mx-40 mb-28">
+            <b className="text-9xl opacity-10">01</b>
+            <b className="text-9xl opacity-10">02</b>
+            <b className="text-9xl opacity-10">03</b>
+          </div>
+          <div className="flex justify-around mx-40">
+            <b className="text-9xl opacity-10">04</b>
+            <b className="text-9xl opacity-10">05</b>
+            <b className="text-9xl opacity-10">06</b>
+          </div>
+        </div>
+      </div>
+      <div name="WhyLifoo" className="py-20 bg-gradient-to-r from-lifoo-100 to-lifoo-300 flex flex-col justify-center">
+        <b className="text-white text-center text-5xl mb-28">
+          Por que entrar<br/> para a Lifoo?
+        </b>
+        <div className="flex flex-row justify-evenly">
+          <div className="flex flex-wrap border bg-lifoo-100 h-56 w-1/5 border-white rounded-full pt-6">
+            <b className="text-7xl text-white opacity-20 ml-10">01</b>
+            <b className="text-white mt-2 text-2xl">Lorem<br/> impsum</b>
+            <p className="text-white mx-10 text-md">
+              Lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum.
+            </p>
+          </div>
+          <div className="flex flex-wrap border bg-lifoo-100 h-56 w-1/5 border-white rounded-full pt-6">
+            <b className="text-7xl text-white opacity-20 ml-10">02</b>
+            <b className="text-white mt-2 text-2xl">Lorem<br/> impsum</b>
+            <p className="text-white mx-10 text-md">
+              Lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum.
+            </p>
+          </div>
+          <div className="flex flex-wrap border bg-lifoo-100 h-56 w-1/5 border-white rounded-full pt-6">
+            <b className="text-7xl text-white opacity-20 ml-10">03</b>
+            <b className="text-white mt-2 text-2xl">Lorem<br/> impsum</b>
+            <p className="text-white mx-10 text-md">
+              Lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum.
+            </p>
+          </div>
+          <div className="flex flex-wrap border bg-lifoo-100 h-56 w-1/5 border-white rounded-full pt-6">
+            <b className="text-7xl text-white opacity-20 ml-10">04</b>
+            <b className="text-white mt-2 text-2xl">Lorem<br/> impsum</b>
+            <p className="text-white mx-10 text-md">
+              Lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum, lorem impsum.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
