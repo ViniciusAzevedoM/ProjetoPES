f554d22c918d30681b8a7a287597a8fd3a2ae970
ViniciusAzevedoM <viniciusm_azevedo@hotmail.com>
2021-12-06 18:40:42 -0300

    footer componetn

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logoVerde from '../images/logoVerde.png'
 
 const Footer = () => {
     return (
         <div name="Footer" className="flex justify-center h-20 bg-white">
-            <p className="self-center text-lifoo-100 text-2xl">Informações do rodapé</p>
+            <img className="h-8 mt-1 self-center" src={logoVerde} alt="logo lifoo branca" />
+            <p className="self-center text-lifoo-100 ml-2 text-md">Copyright © 2021 Lifoo. Todos os direitos reservados.</p>
         </div>
     );
 }
