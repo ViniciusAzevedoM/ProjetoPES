17722ef315d70169107346dc22c1cdc2973f5473
ViniciusAzevedoM <viniciusm_azevedo@hotmail.com>
2021-12-06 18:41:42 -0300

    route to signup page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './index.css';
 import Home from './pages/Home';
+import SingUp from './pages/signup';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SingUp />} />
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
---
+++ src/pages/signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SingUp() {
+  return (
+    <div>
+        <p>Tela de cadastro</p>
+    </div>
+  );
+}
+
+export default SingUp;
