3dd0d8784e207c1f401dde1393d535a551c92023
ViniciusAzevedoM <viniciusm_azevedo@hotmail.com>
2021-12-10 12:41:06 -0300

    updating routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {UserStorage} from './userContext';
 import './index.css';
 import Home from './pages/Home';
 import SingUp from './pages/signup';
-import {UserStorage} from './userContext';
-//import {UserContext} from './userContext';
+import UserScreen from './pages/userScreen';
 
 ReactDOM.render(
   <Router>
@@ -13,6 +13,7 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SingUp />} />
+        <Route path="/userScreen" element={<UserScreen />} />
       </Routes>
     </UserStorage>
   </Router>,
